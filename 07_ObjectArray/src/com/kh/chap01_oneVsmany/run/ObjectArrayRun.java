3978e56a66f9de189407f9a26295da65567427ba
th816 <117423689+th816@users.noreply.github.com>
2022-11-23 10:49:05 +0900

    객체배열 2(오전)

--- 07_ObjectArray/src/com/kh/chap01_oneVsmany/run/ObjectArrayRun.java
+++ 07_ObjectArray/src/com/kh/chap01_oneVsmany/run/ObjectArrayRun.java
@@ -44,14 +44,31 @@ public class ObjectArrayRun {
 		System.out.print("검색할 책 제목 : ");
 		String searchTitle = sc.nextLine();
 		
-		for(int i=0; i<arr.length;i++) {
+		//for(int i=0; i<arr.length;i++) {
 			
+		//	if(arr[i].getTitle().equals(searchTitle)) {
+		//		System.out.println(arr[i].getPrice());
+		//	}
+			
+		//}
+		
+		//만약 일치하는 도서를 찾지 못한경우 "검색한 도서가 없습니다" 출력
+		
+		int count = 0;
+		for(int i=0; i<arr.length; i++) {
 			if(arr[i].getTitle().equals(searchTitle)) {
-				System.out.println(arr[i].getPrice());
+				count++;
+				System.out.println("책의 가격은 : "+arr[i].getPrice());
+				break; // 책의 제목이 중복되지 않은 이상 break문을 걸어주게되면
+					   // 찾고자 하는 책을 찾고나서 더이상 반복문을 실행시키지 않음.
+				
 			}
-			
 		}
 		
+		if(count==0) {
+			
+			System.out.println("검색된 도서가 없습니다");
+		}
 		
 
 	}
